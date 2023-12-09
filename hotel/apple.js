9abb534917bfca05b3ab37953d4eaacb07ca2119
chhunchandevit21 <chhanchandevit21@gmail.com>
2023-12-09 16:08:23 +0700

    first commit

---
+++ hotel/apple.js
@@ -0,0 +1,42 @@
+const menu=[
+    {
+        home:'HOME'
+    },
+    {
+        home:'ASUS'
+    },
+    {
+        home:'DELL'
+    },
+    {
+        home:'MSI'
+    },
+    {
+        home:'APPLE'
+    },
+
+];
+
+var text='';
+for( let i in menu)
+{
+     text +=  `
+
+     <li><a href="http://127.0.0.1:5500/hotel/apple.html">${menu [i]['home']}</a></li>
+     `;
+}
+document.getElementsByClassName('menu')[0].innerHTML= `<ul>${text}</ul>`;
+
+
+
+
+
+
+var text='';
+for( let i in menu)
+{
+    text +=`
+    <li><a href="http://127.0.0.1:5500/hotel/apple.html">${menu [i]['home']}</a></li>
+    `;
+}
+document.getElementsByClassName('menu')[0].innerHTML=` <ul>${text} </ul> `;
